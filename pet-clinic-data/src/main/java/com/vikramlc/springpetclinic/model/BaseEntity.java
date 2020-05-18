e9dc730630dbbb7114df3f3c75b1adc8b0c68fc0
1994 <vikram.chawan@solvezy.com>
2020-05-19 02:24:43 +0530

    Added ID as Long to Models

---
+++ pet-clinic-data/src/main/java/com/vikramlc/springpetclinic/model/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.vikramlc.springpetclinic.model;
+
+public class BaseEntity {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
