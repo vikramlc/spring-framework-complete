ced2b2cb898574acb6a147b29088ee33e8eb0ec6
1994 <vikram.chawan@solvezy.com>
2020-05-18 20:21:50 +0530

    First commit - spring pet clinic

---
+++ src/main/java/com/vikramlc/springpetclinic/SpringpetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.vikramlc.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikramlc/springpetclinic/SpringpetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikramlc.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
