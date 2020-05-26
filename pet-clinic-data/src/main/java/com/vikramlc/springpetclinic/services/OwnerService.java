fd9eebdcd75c246645ae47199344f20cc6919d50
1994 <vikram.chawan@solvezy.com>
2020-05-26 19:19:57 +0530

    Refactoring service interfaces to a base interface. Closes #11

--- pet-clinic-data/src/main/java/com/vikramlc/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/vikramlc/springpetclinic/services/OwnerService.java
@@ -2,15 +2,6 @@ package com.vikramlc.springpetclinic.services;
 
 import com.vikramlc.springpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/vikramlc/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/vikramlc/springpetclinic/services/VetService.java
@@ -2,12 +2,5 @@ package com.vikramlc.springpetclinic.services;
 
 import com.vikramlc.springpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
