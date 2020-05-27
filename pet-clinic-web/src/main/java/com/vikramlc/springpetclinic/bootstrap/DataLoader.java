92df13648d21639405524d5c3e774f08bdf6115a
1994 <vikram.chawan@solvezy.com>
2020-05-28 01:19:23 +0530

    Implemented logic in map to set the ID instead of hardcoding. Closes #24.

--- pet-clinic-web/src/main/java/com/vikramlc/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/vikramlc/springpetclinic/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Max");
         owner1.setLastName("Miller");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Mary");
         owner2.setLastName("Jane");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Billy");
         vet1.setLastName("Mayor");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Sheldon");
         vet2.setLastName("Cooper");
 
