0e8ca536b7e73bb116293068868a938340f40e51
1994 <vikram.chawan@solvezy.com>
2020-06-02 00:39:38 +0530

    Added Speciality Service and Implementation. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/vikramlc/springpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.vikramlc.springpetclinic.services;
+
+import com.vikramlc.springpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
