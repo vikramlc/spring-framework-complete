07d52f3073d0d1d4bf440c586c58935f9e08d935
1994 <vikram.chawan@solvezy.com>
2020-06-03 17:42:39 +0530

    Added visit with Jpa entity mappings. Closes #38.

--- pet-clinic-data/src/main/java/com/vikramlc/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/vikramlc/springpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.vikramlc.springpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
