c663c3411bdb777fdae4bea96b5aa5c8a1d967a3
1994 <vikram.chawan@solvezy.com>
2020-06-03 17:30:09 +0530

    Updated vets with JPA entity mappings. Closes #37.

--- pet-clinic-data/src/main/java/com/vikramlc/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/vikramlc/springpetclinic/model/Pet.java
@@ -18,7 +18,7 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
 
-    @Column(name = "birthDate")
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
